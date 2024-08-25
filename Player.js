49c324a9fc839a0c85f2f767f5a73262e1edb4f0
unknown <alexandreliberatto@gmail.com>
2024-08-25 16:51:45 -0300

    Criando footer e estilizando

--- Player.js
+++ Player.js
@@ -7,9 +7,37 @@ import {AntDesign} from '@expo/vector-icons';
 
 export default function Player() {
     return (
-      <View>
-        <Text>Player Component</Text>
+      <View style={styles.player}>
+        <TouchableOpacity style={styles.touch}>
+            <AntDesign name= "banckward" size={35} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.touch}>
+            <AntDesign name= "playcircleo" size={35} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.touch}>
+            <AntDesign name= "forward" size={35} color="white" />
+        </TouchableOpacity>
       </View>
     );
   }
+
+  const styles = StyleSheet.create({
+    player: {
+        width: '100%',
+        height: 100,
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        zIndex: 999,
+        backgroundColor: '#111',
+        alignItems: 'center',   // Centraliza no eixo cruzado (vertical)
+        justifyContent: 'center', // Centraliza no eixo principal (horizontal)
+        flexDirection: 'row',
+    },    
+    touch: {
+        marginRight: 20,
+        marginLeft: 20,
+    }
+        
+  })
   
